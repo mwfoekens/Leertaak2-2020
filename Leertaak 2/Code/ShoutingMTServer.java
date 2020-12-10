ad002b864611090c78b9d807a9da9c0c151e6e88
Merel Foekens <mwfoekens@gmail.com>
2020-12-10 21:50:00 +0100

    Hoeft alleen nog naar db te schrijven en data correctie

--- Leertaak 2/Code/ShoutingMTServer.java
+++ Leertaak 2/Code/ShoutingMTServer.java
@@ -3,8 +3,12 @@ package Code;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ShoutingMTServer {
     public static final int PORT = 2500;
@@ -12,60 +16,18 @@ public class ShoutingMTServer {
     public static TelSemafoor mijnSemafoor = new TelSemafoor(maxnrofConnections);
 
 
-    public static void main(String[] args) throws InterruptedException, JAXBException {
-        String measurement = "<?xml version=\"1.0\"?>\n" +
-                "<WEATHERDATA>\n" +
-                "<MEASUREMENT>\n" +
-                "    <STN>123456</STN>\n" +
-                "    <DATE>2009-09-13</DATE>\n" +
-                "    <TIME>15:59:46</TIME>\n" +
-                "    <TEMP>-60.1</TEMP>\n" +
-                "    <DEWP>-58.1</DEWP>\n" +
-                "    <STP>1034.5</STP>\n" +
-                "    <SLP>1007.6</SLP>\n" +
-                "    <VISIB>123.7</VISIB>\n" +
-                "    <WDSP>10.8</WDSP>\n" +
-                "    <PRCP>11.28</PRCP>\n" +
-                "    <SNDP>11.1</SNDP>\n" +
-                "    <FRSHTT>010101</FRSHTT>\n" +
-                "    <CLDC>87.4</CLDC>\n" +
-                "    <WNDDIR>342</WNDDIR>\n" +
-                "</MEASUREMENT>\n" +
-                "<MEASUREMENT>\n" +
-                "    <STN>123456</STN>\n" +
-                "    <DATE>2009-09-13</DATE>\n" +
-                "    <TIME>15:59:46</TIME>\n" +
-                "    <TEMP>-60.1</TEMP>\n" +
-                "    <DEWP>-58.1</DEWP>\n" +
-                "    <STP>1034.5</STP>\n" +
-                "    <SLP>1007.6</SLP>\n" +
-                "    <VISIB>123.7</VISIB>\n" +
-                "    <WDSP>10.8</WDSP>\n" +
-                "    <PRCP>11.28</PRCP>\n" +
-                "    <SNDP>11.1</SNDP>\n" +
-                "    <FRSHTT>010101</FRSHTT>\n" +
-                "    <CLDC>87.4</CLDC>\n" +
-                "    <WNDDIR>342</WNDDIR>\n" +
-                "</MEASUREMENT>\n" +
-                "</WEATHERDATA>";
-        InputStream targetStream = new ByteArrayInputStream(measurement.getBytes());
-        JAXBContext jaxbContext = JAXBContext.newInstance(WeatherData.class);
-
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        WeatherData test = (WeatherData) jaxbUnmarshaller.unmarshal(targetStream);
-        System.out.println(test);
-//        Socket connection;
+    public static void main(String[] args) throws InterruptedException, JAXBException, IOException {
+        Socket connection;
 //        Thread dbThread = new Thread(new DBThread());
 //        dbThread.start();
-//        TimeUnit.SECONDS.sleep(5);
-//            ServerSocket server = new ServerSocket(PORT);
-//            System.err.println("MT Server started.. bring on the load, to a maximum of: " + maxnrofConnections);
-//
-//            while (true) {
-//                connection = server.accept();
-//                System.err.println("New connection accepted..handing it over to worker thread");
-//                Thread worker = new Thread(new Worker(connection));
-//                worker.start();
-//            }
+        ServerSocket server = new ServerSocket(PORT);
+        System.err.println("MT Server started.. bring on the load, to a maximum of: " + maxnrofConnections);
+
+        while (true) {
+            connection = server.accept();
+            System.err.println("New connection accepted..handing it over to worker thread");
+            Thread worker = new Thread(new Worker(connection));
+            worker.start();
+        }
     }
 }
--- Leertaak 2/Code/ShoutingThread.java
+++ Leertaak 2/Code/ShoutingThread.java
@@ -1,5 +1,8 @@
 package Code;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.net.*;
 import java.io.*;
 //import java.util.concurrent.*;
@@ -19,20 +22,35 @@ class Worker implements Runnable {
             //lets check if we already accepted maximum number of connections
             ShoutingMTServer.mijnSemafoor.probeer();
 
-            PrintWriter printWriter = new PrintWriter(connection.getOutputStream(), true);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            while ((s = bufferedReader.readLine()) != null) {
-//                printWriter.println(s.toUpperCase());
-                System.out.println(s);
+            JAXBContext jaxbContext = JAXBContext.newInstance(WeatherData.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            StringBuilder builder = null;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.startsWith("<?xml")) {
+                    if (builder != null) {
+                        WeatherData q = (WeatherData) jaxbUnmarshaller.unmarshal(new ByteArrayInputStream(builder.toString().getBytes()));
+                        System.out.println(q); // TODO zet op queue + data correctie
+                    }
+                    builder = new StringBuilder();
+                }
+                builder.append(line);
             }
+            WeatherData q = (WeatherData) jaxbUnmarshaller.unmarshal(new ByteArrayInputStream(builder.toString().getBytes()));
+
+            System.out.println(q); // TODO zet op queue + data correctie
+
 
             // now close the socket connection
             connection.close();
             System.err.println("Connection closed: workerthread ending");
             // upping the semaphore.. since the connnection is gone....
             ShoutingMTServer.mijnSemafoor.verhoog();
-        } catch (IOException | InterruptedException ioe) {
+        } catch (IOException | InterruptedException | JAXBException ioe) {
         }
     }
 }
